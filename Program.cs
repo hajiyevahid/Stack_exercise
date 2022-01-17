1f938de7fc80528a29ec2b474cd613a0c2afc520
hajiyevahid <vahid.hajiyev@stud.vgtu.lt>
2022-01-17 14:56:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Stack_exercise
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Contains("Pop"))
+                {
+                    stack.Pop();
+                }
+
+                else if (input.Contains("Push"))
+                {
+                    var popped_element = input.Substring(4);
+                    stack.Push(popped_element);
+                }
+
+                else if (input.Contains("Clear"))
+                {
+                    stack.Clear();
+                    Console.WriteLine("All elements in stack is cleared !");
+
+                }
+                else
+                {
+                    Console.WriteLine("Please operate a valid operation onto stack !");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace Stack_exercise 
+{
+    public class Stack
+    {
+        List<object> list = new List<object>();
+ 
+
+        public Stack()
+        {
+            //list = new List<object>();
+        }
+        public void Push(object obj)
+        {
+
+            try
+            {
+                list.Add(obj);
+
+                for (int i = list.Count - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(list[i]);
+                }
+
+            }
+            catch (System.InvalidOperationException)
+            {
+                System.Console.WriteLine("Please don't add null element to stack !");
+
+            }
+        }
+
+        public void Pop()
+        {
+            try
+            {
+
+                list.RemoveAt(list.Count - 1);
+                for (int i = list.Count - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(list[i]);
+                }
+            }
+            catch (System.NullReferenceException)
+            {
+                System.Console.WriteLine("Please add at least 1 element to remove it from stack !");
+
+            }
+
+        }
+
+        public void Clear()
+        {
+            if (list != null)
+            {
+                list.Clear();
+            }
+            else
+            {
+                System.Console.WriteLine("List is empty !");
+            }
+
+        }
+    }
+}
